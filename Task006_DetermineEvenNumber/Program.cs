88d0a6b6cb8f865ea52665f02d2902d7783fe920
Akuloff.M <Akuloff.M@gmail.com>
2022-02-06 19:26:51 +0300

    Add Task 006 Determine Even Number project

---
+++ Task006_DetermineEvenNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+
+int number = new Random().Next(1, 100);
+Console.WriteLine("Сгенерированное число: " + number);
+
+int remains = number % 2;
+
+if(remains == 0)
+{
+    Console.WriteLine("Является чётным");
+}
+else
+{
+    Console.WriteLine("Является нечётным");
+}
