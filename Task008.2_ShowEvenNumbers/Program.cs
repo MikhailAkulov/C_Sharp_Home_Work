f78dd74db9fc11b1393ec3571198f9217fbabe8d
Akuloff.M <Akuloff.M@gmail.com>
2022-02-08 22:51:01 +0300

    Add Task 008.2 Show Even Numbers project

---
+++ Task008.2_ShowEvenNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N (Работа над замечаниями)
+
+Console.Write("Введите число N: ");
+string? numN = Console.ReadLine();
+int number = int.Parse(numN);
+int min = 2;
+while (min < number)
+{
+    if (min % 2 == 0)
+        Console.Write(min + " ");
+        min++;
+}
+if (number < 0)
+    Console.Write("Ввели отрицательное число, попробуйте ещё разок");
