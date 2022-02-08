dc7449d60a6ce828d73ee6f25e9f1cdcd018faa7
Akuloff.M <Akuloff.M@gmail.com>
2022-02-08 22:10:15 +0300

    Add Task 004.2 Search Max project

---
+++ Task004.2_SearhMax/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти максимальное из трех чисел (Исправление замечаний)
+
+Random randomizer = new Random();
+int a = randomizer.Next(1, 100);
+int b = randomizer.Next(1, 100);
+int c = randomizer.Next(1, 100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+Console.WriteLine(); // Пустая строка для отделения ответа
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
