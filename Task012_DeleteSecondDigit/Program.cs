d2d45c888fa3dbb69400a8e42df2169d376439fa
Akuloff.M <Akuloff.M@gmail.com>
2022-02-09 23:30:31 +0300

    Add Task 012 delete second digit project

---
+++ Task012_DeleteSecondDigit/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 12. Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+Console.WriteLine($"Случайное трёхзначное число: {number}");
+Console.WriteLine($"Число без второй цифры: {firstDigit}{thirdDigit}");
