8a734a018985a49c1273837e5d4303dc0ea7979b
Akuloff.M <Akuloff.M@gmail.com>
2022-02-09 22:56:18 +0300

    Add Task 010.2 Show second digit project

---
+++ Task010.2_ShowSecondDigit/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10. Показать вторую цифру трёхзначного числа. (С рандомайзером и попроще)
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число: {number}");
+
+int secondDigit = number % 100 / 10;
+Console.WriteLine($"Вторая цифра числа: {secondDigit}");
