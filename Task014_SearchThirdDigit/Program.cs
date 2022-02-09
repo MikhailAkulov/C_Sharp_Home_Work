d7b1bbdc5d5c04225e3f6449d02a1e3f2472fdf5
Akuloff.M <Akuloff.M@gmail.com>
2022-02-10 00:41:43 +0300

    Add Task 014 search third digit project

---
+++ Task014_SearchThirdDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 14. Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите трёхзначное число число: ");
+
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int result))
+{
+    string number = result.ToString();
+    if(number.Length >= 3)
+        Console.WriteLine($"Третья цифра числа: {number[2]}");
+    else
+        Console.WriteLine("Третьей цифры нет, может число не трёхзначное? Попробуйте ещё разок");
+}
+else
+    Console.WriteLine("Ввели не число");
