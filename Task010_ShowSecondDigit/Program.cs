b2bd6ff908ead0bb1b34c198bdb647f0fd9d624e
Akuloff.M <Akuloff.M@gmail.com>
2022-02-09 22:34:41 +0300

    Add Task 010 Show second digit project

---
+++ Task010_ShowSecondDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10. Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трёхзначное число число: ");
+
+string valueStr = Console.ReadLine();
+
+if (int.TryParse(valueStr, out int valueInt))
+{
+    string resultValue = valueInt.ToString();
+    if(resultValue.Length == 3)
+        Console.WriteLine($"Вторая цифра числа: {resultValue[1]}");
+    else
+        Console.WriteLine("Число не трёхзначное");
+}
+else
+    Console.WriteLine("Ввели не число");
