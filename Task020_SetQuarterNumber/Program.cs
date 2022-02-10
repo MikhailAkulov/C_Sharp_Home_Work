31209570b1d1c7bb67a9f588841c55f6c50e3946
Akuloff.M <Akuloff.M@gmail.com>
2022-02-10 20:02:12 +0300

    Add Task 020 set quarter number project

--- Task020_SetQuarterNumber/Program.cs
+++ Task020_SetQuarterNumber/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Задача 20. Задать номер четверти, показать диапазоны для возможных координат
 
+int quarterPlane = new Random().Next(1, 5);
+Console.WriteLine($"Случайный номер четверти: {quarterPlane}");
+
+if (quarterPlane == 1)
+    Console.WriteLine("I четверть, Диапазон возможных координат: (x>0, y>0)");
+else if (quarterPlane == 2)
+    Console.WriteLine("II четверть, Диапазон возможных координат: (x<0, y>0)");
+else if (quarterPlane == 3)
+    Console.WriteLine("III четверть, Диапазон возможных координат: (x<0, y<0)");
+else if (quarterPlane == 4)
+    Console.WriteLine("IV четверть, Диапазон возможных координат: (x>0, y<0)");
