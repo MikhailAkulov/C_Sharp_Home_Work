cbff83f77d95519eb3a1307f22a6ee811b1060aa
Akuloff.M <Akuloff.M@gmail.com>
2022-02-10 22:54:46 +0300

    Add Task 022 find distance  project

---
+++ Task022_FindDistance(2D_3D)/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 22. Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine();
+Console.ReadLine();
+
+Random r = new Random();
+
+var temp = Math.
+
+var temp = Math.Pow(2,4); // var x = 2*2*2*2;
