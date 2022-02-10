a61cfb32a39e0943a888106f94ad638b6a4f33fa
Akuloff.M <Akuloff.M@gmail.com>
2022-02-10 19:27:32 +0300

    Add Task 024 cubes of numbers project

---
+++ Task024_CubesOfNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 24. Найти кубы чисел от 1 до N
+
+Console.Write("Введите число N: ");
+string? numN = Console.ReadLine();
+int number = int.Parse(numN);
+int min = 1;
+while (min < number)
+{
+    if (min > 0)
+        Console.Write(((min+1) * (min+1) * (min+1)) + " ");
+    min++;
+}
+if (number < 0)
+    Console.Write("Ввели отрицательное число, попробуйте ещё разок");
