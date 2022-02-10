d5cc8724c516b55afe0120ac060f30605ad8b917
Akuloff.M <Akuloff.M@gmail.com>
2022-02-10 17:03:02 +0300

    Add Task 016 find weekend project

--- Task016_FindWeekEnd/Program.cs
+++ Task016_FindWeekEnd/Program.cs
@@ -1,24 +1,22 @@
 ﻿// Задача 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 
-string inPut = String.Empty;
+string numberStr = String.Empty;
 int number = 0;
 while (number <= 0 || number >= 8)
 {
     Console.Write("Введите число от 1 до 7: ");
-    inPut = Console.ReadLine();
-    int.TryParse(inPut, out number);
+    numberStr = Console.ReadLine();
+    int.TryParse(numberStr, out number);
 }
 
-string[] dayOfWeek = { "Чётров Monday", "Гребаный Tuesday", "Долбаная Wednesday", "Ещё чуть-чуть, Thursday", "Слава Богу, Friday", "Ура, Saturday!", "Sunday, блин, завтра понедельник" };
-//                           0                  1                   2                           3                       4               5                           6
+string[] dayOfWeek = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+//                           0            1         2         3          4          5            6
 
 if (number == 6 || number == 7)
 {
-    Console.WriteLine($"{number} - выходной день");
+    Console.WriteLine($"{number} - {dayOfWeek[number - 1]}: выходной день");
 }
 else
 {
-    Console.WriteLine($"{number} - будний день");
+    Console.WriteLine($"{number} - {dayOfWeek[number - 1]}: будний день");
 }
-
-Console.WriteLine(dayOfWeek[number - 1]);
