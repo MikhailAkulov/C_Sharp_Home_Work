aba171f8d09e443e9b2649d1dfb99caa28733c39
Akuloff.M <Akuloff.M@gmail.com>
2022-02-17 18:21:31 +0300

    Add Task 030 Show cubes of even numbers project

---
+++ Task030_ShowCubesEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+bool result = true;
+string s = string.Empty;
+double number = 0;
+
+while (number <= 0)
+{
+    Console.Write("Введите число > 1: ");
+    s = Console.ReadLine();
+    result = double.TryParse(s, out number);
+}
+
+for (double i = 0; i <= number; i++)
+{
+    double cube = Math.Pow(i, 3);
+    double even = cube % 10;
+
+    if (even % 2 == 0 && even != 0)
+    {
+        Console.WriteLine($"{i}^3 = {cube}");
+    }
+}
