b52f9cb59dcce773995571b6e3bd3845171ed3c4
Akuloff.M <Akuloff.M@gmail.com>
2022-02-17 17:22:30 +0300

    Add Task 028 Sum Of Digits project

---
+++ Task028_SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28. Подсчитать сумму цифр в числе
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(10, 1000000);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine($"Случайное число: {number}");
+
+int temporary = number;
+int SumOfDigits = 0;
+while (temporary != 0)
+{
+    SumOfDigits += temporary % 10;
+    temporary /= 10;
+}
+
+Console.WriteLine($"Сумма цифр случайного числа: {SumOfDigits}");
