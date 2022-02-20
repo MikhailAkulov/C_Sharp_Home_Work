d1db81f3e100dc6ae4f150b3d9473236b0f80c31
Akuloff.M <Akuloff.M@gmail.com>
2022-02-21 02:08:04 +0300

    Add Task 038 sum of the array numbers standing in an odd position project

---
+++ Task038_SumOddPositions/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Random randomizer = new Random();
+int a = randomizer.Next(5, 11);
+int[] array = new int[a];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 11);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+void CheckingArray(int[] arr)
+{
+    int[] checkingArray = new int[arr.Length];
+    int sumOddPositions = 0;
+    int index = 1;
+    while (index < arr.Length)
+    {
+        sumOddPositions += arr[index];
+        index = index + 2;
+    }
+    Console.Write($"Cумма чисел на нечётных позициях массива = {sumOddPositions}");
+}
+
+Console.Write("Массив случайных чисел: ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+CheckingArray(array);
