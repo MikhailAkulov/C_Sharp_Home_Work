d7b57a8498800c3c783ee313cf5fc7a4fbd35fab
Akuloff.M <Akuloff.M@gmail.com>
2022-02-28 02:55:06 +0300

    Add Task054 Find the sum of the elements of the main diagonal of the matrix project

---
+++ Task054_SumMainDiagonalMatrix/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 54. В матрице чисел найти сумму элементов главной диагонали
+
+int[,] array = new int[6, 6];
+int sum = 0;
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+                sum = sum + array[i, j];
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Заданная матрица чисел:");
+FillArray();
+PrintArray();
+Console.WriteLine($"Сумма главной диагонали матрицы = {sum}");
