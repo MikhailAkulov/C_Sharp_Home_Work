10725e978de73ccdbb7e4d57ccec78090dc186b5
Akuloff.M <Akuloff.M@gmail.com>
2022-02-28 01:12:14 +0300

    Add Task048_Show a two-dimensional array of size MxN filled with integers project

---
+++ Task048_ShowTwoDimensionalArray/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+Console.Clear();
+
+bool result = true;
+string numberStrM = string.Empty;
+string numberStrN = string.Empty;
+int numberM = 0;
+int numberN = 0;
+
+while (numberM <= 0)
+{
+    Console.Write("Введите количество строк массива: ");
+    numberStrM = Console.ReadLine();
+    result = int.TryParse(numberStrM, out numberM);
+}
+
+while (numberN <= 0)
+{
+    Console.Write("Введите количество столбцов массива: ");
+    numberStrN = Console.ReadLine();
+    result = int.TryParse(numberStrN, out numberN);
+}
+
+int[,] array = new int[numberM,numberN];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Двумерный массив по заданным параметрам:");
+FillArray();
+PrintArray();
