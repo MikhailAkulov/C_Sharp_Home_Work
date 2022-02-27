109d26b32cec5e87f85dd2b5048d86551ada2977
Akuloff.M <Akuloff.M@gmail.com>
2022-02-28 02:20:26 +0300

    Add Task052 In a two-dimensional array, replace the elements whose both indexes are even with their squares project

---
+++ Task052_SquareNumbersWithBothEvenIndexes/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+double[,] array = new double[6, 8];
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(4, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SquareElementsWith2EvenIndexes()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = Math.Pow(array[i, j], 2);
+            }
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Первоначальный вид массива:");
+FillArray();
+PrintArray();
+Console.WriteLine("Массив в котором элементы у которых оба индекса четные, возведены в квардат:");
+SquareElementsWith2EvenIndexes();
